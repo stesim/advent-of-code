77cd31b6e4db9b1ef59da6b5247d7aef7eba57d4
stesim <simeonov.stefan@gmail.com>
2020-12-10 02:24:52 +0100

    clean-up of day 4

--- Day04.hs
+++ Day04.hs
@@ -23,69 +23,58 @@ requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 parsePassport :: Text -> Passport
 parsePassport string = fmap parseEntry entries
-  where
-    entries = P.filter (not . T.null) $ split isSpace string
+  where entries = P.filter (not . T.null) $ split isSpace string
 
 parseEntry :: Text -> (Text, Text)
 parseEntry string = (name, T.tail valueStr)
-  where
-    (name, valueStr) = T.break (== ':') string
+  where (name, valueStr) = T.break (== ':') string
 
 isPassportValid1 :: Passport -> Bool
 isPassportValid1 entries = P.all (`elem` entryNames) requiredFields
-  where
-    entryNames = fmap fst entries
+  where entryNames = fmap fst entries
 
 isPassportValid2 :: Passport -> Bool
 isPassportValid2 entries = P.all validate requiredFields
-  where
-    validate name = let entry = L.find (\(name', _) -> name' == name) entries
-                    in (validateEntry <$> entry) == Just True
+  where validate name = let entry = L.find (\(name', _) -> name' == name) entries
+                        in (validateEntry <$> entry) == Just True
 
 validateEntry :: PassportEntry -> Bool
-validateEntry entry = case entry of
-  ("byr", y) -> validateYear 1920 2002 y
-  ("iyr", y) -> validateYear 2010 2020 y
-  ("eyr", y) -> validateYear 2020 2030 y
-  ("hgt", h) -> validateHeight h
-  ("hcl", c) -> validateHairColor c
-  ("ecl", c) -> validateEyeColor c
-  ("pid", i) -> validatePassportID i
-  ("cid", i) -> validateCountryID i
+validateEntry (key, value) = validator value
+  where validator = case key of
+                      "byr" -> validateIntRange 1920 2002
+                      "iyr" -> validateIntRange 2010 2020
+                      "eyr" -> validateIntRange 2020 2030
+                      "hgt" -> validateHeight
+                      "hcl" -> validateHexColor
+                      "ecl" -> validateColorString
+                      "pid" -> validatePassportID
+                      "cid" -> const True
+                      _     -> const False
 
-validateYear :: Int -> Int -> Text -> Bool
-validateYear min max year = year' `isInRange` (min, max)
-  where year' = readT year
-
-validHeightUnits :: [Text]
-validHeightUnits = ["cm", "in"]
+validateIntRange :: Int -> Int -> Text -> Bool
+validateIntRange min max value = readT value `isInRange` (min, max)
 
 validateHeight :: Text -> Bool
-validateHeight height = unitIsValid && magnitude `isInRange` validRange
-  where
-    (magnitudeText, unit) = T.span isDigit height
-    magnitude = readT magnitudeText
-    unitIsValid = unit `elem` validHeightUnits
-    validRange = case unit of
-      "cm" -> (150, 193)
-      "in" -> (59, 76)
+validateHeight height = (isInRange magnitude <$> range) == Just True
+  where (magnitude', unit) = T.span isDigit height
+        magnitude = readT magnitude'
+        range = case unit of
+          "cm" -> Just (150, 193)
+          "in" -> Just (59, 76)
+          _    -> Nothing
 
-validateHairColor :: Text -> Bool
-validateHairColor color = T.length color == 7 &&
-                          T.head color == '#' &&
-                          T.all (\c -> isDigit c || c `elem` ['a' .. 'f']) (T.tail color)
+validateHexColor :: Text -> Bool
+validateHexColor color = T.length color == 7 &&
+                         T.head color == '#' &&
+                         T.all (`elem` validChars) (T.tail color)
+  where validChars = ['0'..'9'] ++ ['a'..'f']
 
-validEyeColors :: [Text]
-validEyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-
-validateEyeColor :: Text -> Bool
-validateEyeColor color = color `elem` validEyeColors
+validateColorString :: Text -> Bool
+validateColorString color = color `elem` validColors
+  where validColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 
 validatePassportID :: Text -> Bool
 validatePassportID id = T.length id == 9 && T.all isDigit id
-
-validateCountryID :: Text -> Bool
-validateCountryID _ = True
 
 isInRange :: Int -> (Int, Int) -> Bool
 isInRange n (min, max) = n >= min && n <= max
