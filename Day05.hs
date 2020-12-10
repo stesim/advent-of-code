8c867809b2a333b7d8d9ab3bf4744f5d72e7fe0f
stesim <simeonov.stefan@gmail.com>
2020-12-10 23:54:24 +0100

    clean-up of day 5

--- Day05.hs
+++ Day05.hs
@@ -9,18 +9,20 @@ type Aisle = Int
 
 main :: IO ()
 main = do
-    seats <- sort . fmap parseTicket . lines <$> readFile "Day05_input.txt"
-    let highestSeat = last seats
-    let seatNumDiff = zipWith (\s s' -> (s, s' - s)) seats (tail seats)
-    let Just (missingSeatPre, _) = find ((>1) . snd) seatNumDiff
-    putStr "Part one: " >> print highestSeat
-    putStr "Part two: " >> print (succ missingSeatPre)
+  (solution1, solution2) <- solve <$> readFile "Day05_input.txt"
+  putStr "Part one: " >> print solution1
+  putStr "Part two: " >> print solution2
+
+solve :: String -> (Int, Int)
+solve input = (last seats, succ missingSeatPre)
+  where seats = sort . fmap parseTicket . lines $ input
+        seatNumDiff = zipWith (\s s' -> (s, s' - s)) seats (tail seats)
+        Just (missingSeatPre, _) = find ((>1) . snd) seatNumDiff
 
 parseTicket :: Ticket -> Seat
-parseTicket ticket =
-    let (row, aisle) = span (`elem` "FB") ticket
+parseTicket ticket = seats * parseRow row + parseAisle aisle
+  where (row, aisle) = span (`elem` "FB") ticket
         seats = 2^length aisle
-    in seats * parseRow row + parseAisle aisle
 
 parseRow :: String -> Row
 parseRow = bspStringParse 'F' 'B'
@@ -30,12 +32,11 @@ parseAisle = bspStringParse 'L' 'R'
 
 bspStringParse :: Char -> Char -> String -> Int
 bspStringParse cl cr = bspParse . fmap binarify
-    where binarify c
-            | c == cl = 0
-            | c == cr = 1
+  where binarify c
+          | c == cl = 0
+          | c == cr = 1
 
 bspParse :: [Int] -> Int
 bspParse []    = 0
-bspParse input =
-    let offset = 2^(length input - 1)
-    in head input * offset + bspParse (tail input)
+bspParse input = head input * offset + bspParse (tail input)
+  where offset = 2^(length input - 1)
